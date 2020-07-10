33713d41c391f6d9a040ad6fa5052bf376f3bd89
Divij Shrivastava <divij@Divijs-MacBook-Pro.local>
2020-07-10 16:20:36 +0530

    Building a sticky navigation - Part 2

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
  
+    var waypoint = new Waypoint({
+        element: $('.js--section-features'),
+    handler:function(direction){
+        if(direction == "down"){
+            $("nav").addClass("sticky");
+        }else{
+            $("nav").removeClass("sticky");
+        }
+    },
+		offset:'60px;'
 });
+    
+})
+
